a3ee4f993a3aeafc537b005da94b5645b453e980
Edward Dal Santo <ejds001@gmail.com>
2016-04-26 11:49:50 -0400

    email set for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,21 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+    # email enabled in production
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    user_name:  Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
